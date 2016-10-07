9d67e311e682aea78942bfd67a43b17ec9d4257f
Westermann <westermann93@googlemail.com>
2016-10-08 01:20:06 +0200

    update player class

--- gameserver/player.js
+++ gameserver/player.js
@@ -2,8 +2,17 @@
 
 class Player {
     
-    constructor() {
-        console.log("Created player");
+    constructor(side, name, id) {
+        this.side = side
+        this.name = name
+        this.id = id
+        this.lanes = Array(NUM_LANES).fill().map((_, i) => {
+            return new lane(i)
+        })
+    }
+
+    spawnUnit(lane) {
+        this.lanes[lane].addUnit()
     }
 }
 
