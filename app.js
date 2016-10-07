2087fadc188926f00187d1902edb66b8fed768ae
Jonathan Balls <j.balls@btopenworld.com>
2016-10-07 21:47:26 +0200

    Copied basic express structure from the stranded

--- app.js
+++ app.js
@@ -2,3 +2,28 @@
 
 console.log("basically running");
 
+
+var port            = process.env.PORT || 4004,
+    app             = require('express')(),
+    server          = require('http').Server(app),
+    io              = require('socket.io')(server),
+    verbose         = false,
+    update_delta    = 30; //ms
+
+// Process command line arguments;
+process.argv.forEach(function (val, index, array) {
+    if (val == "-noZombies") {
+        createZombies = false;
+    }
+});
+
+// Start server.
+server.listen(gameport);
+console.log(':: Listening on port ' + gameport);
+
+
+// Serve 'index.html' at the root.
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
