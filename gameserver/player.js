1d97c57b27785ee76c6b28741574ecad7dd363ab
Westermann <westermann93@googlemail.com>
2016-10-08 01:52:12 +0200

    Add game on player

--- gameserver/player.js
+++ gameserver/player.js
@@ -5,17 +5,22 @@ var NUM_LANES = 2
 
 class Player {
     
-    constructor(side, name, id) {
-        this.side = side
+    constructor(game, name, id) {
+        this.game = game
         this.name = name
         this.id = id
+        this.health = 100
         this.lanes = Array(NUM_LANES).fill().map((_, i) => {
             return new lane(i)
         })
     }
 
-    spawnUnit(lane) {
-        this.lanes[lane].addUnit()
+    spawnUnit(lane, type) {
+        this.lanes[lane].addUnit(type)
+    }
+
+    takeDamage(damage) {
+        this.health = this.health - damage
     }
 }
 
