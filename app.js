8a36958647d2a4c41cf0e19bcab58bdd859ba114
Jonathan Balls <j.balls@btopenworld.com>
2016-10-07 23:17:12 +0200

    Created skeleton classes for game and player

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 "use strict";
 
-console.log("basically running");
-
+console.log("Double Trouble v0.0.1 running");
 
 var gameport        = process.env.PORT || 4004,
     app             = require('express')(),
     server          = require('http').Server(app),
     io              = require('socket.io')(server),
+    Game            = require('./gameserver/game.js'),
+    Player          = require('./gameserver/player.js'),
     verbose         = false,
-    update_delta    = 30; //ms
+    update_delta    = 30, //ms
+    ids_given       = 0,
+    players         = [],
+    games           = []; // No players playing no games
 
 // Process command line arguments;
 process.argv.forEach(function (val, index, array) {
@@ -17,21 +21,44 @@ process.argv.forEach(function (val, index, array) {
     }
 });
 
+
 // Start server.
 server.listen(gameport);
 console.log(':: Listening on port ' + gameport);
 
 
-// Serve 'index.html' at the root.
+// Serve 'index.html' at the root and serve static files at /static/
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
-
-// Serve static files
 app.get('/static/*', function(req, res, next) {
     var file = req.params[0];
 
     if(verbose) console.log('\t :: Express :: file requested : ' + file);
 
-    res.sendfile(__dirname + '/static/' + file);
+    res.sendFile(__dirname + '/static/' + file);
 });
+
+
+// Client connection algo
+io.on('connection', function(socket) {
+    socket.userid = ids_given++;
+    socket.score = 0;
+
+    // Assign them a unique ID.
+    socket.emit( 'userid', { id: socket.userid } );
+    console.log('Player ' + socket.userid + ' connected.');
+
+    // Handle the user disconnecting.
+    socket.on('disconnect', function() {
+        var num_users = collect_userstates().length;
+        if (!num_users && zombies.length) {
+            zombies = [];
+            console.log('No users present. Deleting all zombies');
+        }
+
+        console.log('Player ' + socket.userid + ' disconnected');
+    });
+});
+
+
---
+++ gameserver/game.js
@@ -0,0 +1,11 @@
+"use strict"
+
+class Game {
+    
+    constructor() {
+        console.log("Created game");
+    }
+}
+
+module.exports = Game;
+
