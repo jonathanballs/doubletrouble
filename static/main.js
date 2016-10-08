0dd4e35d728ef4bf98c850a7d55ea331b23f8f57
Pedro Mendonca <peumendonca@gmail.com>
2016-10-08 03:58:24 +0200

    addded mirror support

--- static/main.js
+++ static/main.js
@@ -1,5 +1,6 @@
 //setup
 var bodyRef = document.body;
+var player = 0;
 var renderer = PIXI.autoDetectRenderer(window.innerWidth,window.innerHeight, {antialias:false, transparent:false, resolution:1});
 document.body.appendChild(renderer.view);
 
@@ -10,6 +11,8 @@ PIXI.loader
     .add("static/assets/main/blTile.png")
     .add("static/assets/main/brTile.png")
     .add("static/assets/main/lrTile.png")
+    .add("static/assets/main/ltTile.png")
+    .add("static/assets/main/lbTile.png")
     .add("static/assets/main/lSplit.png")
     .add("static/assets/main/rSplit.png")
     .add("static/assets/main/tbTile.png")
@@ -37,41 +40,68 @@ function setup()
     //make BG dynamically
     //make map for left side
     var init = new Array();
-    init[0] =  window.innerWidth*0.2;
+    if (player == 0)
+    {
+        init[0] =  window.innerWidth*0.2;
+    }else{
+        init[0] =  window.innerWidth*0.8;
+    }
     init[1] = makeCastle(init[0]);
     //make castle and assign the initial x
     makeRoads(init);    
+    makeHuts(init);
 
     //render
     renderer.render(stage);
 }
 function makeSprite(x, y, sprite)
 {
-    var tmp = new PIXI.Sprite(PIXI.loader.resources["static/assets/main/"+sprite+".png"].texture)
+    var tmp = new PIXI.Sprite(PIXI.loader.resources["static/assets/main/"+sprite+".png"].texture);
     tmp.x = x;
     tmp.y = y;
     stage.addChild(tmp);
     return tmp;
 }
-function makeHuts()
+function makeHuts(init)
 {
     var huts = new Array();
-    huts.push(makeSprite(init[0]+256+32,init[1]-256+32,"house"));
-    huts.push(makeSprite(init[0]+256+32,init[1]+256+32,"house"));
+    if(player == 0)
+    {
+        huts.push(makeSprite(init[0]+256+32,init[1]-256+32,"house"));
+        huts.push(makeSprite(init[0]+256+32,init[1]+256+32,"house"));
+    }else{
+        huts.push(makeSprite(init[0]-384+32,init[1]-256+32,"house"));
+        huts.push(makeSprite(init[0]-384+32,init[1]+256+32,"house"));
+    }
 }
 function makeRoads(init)
 {
     var roads = new Array();
-    roads.push(makeSprite(init[0]+128,init[1],"lrTile"));
-    roads.push(makeSprite(init[0]+256,init[1],"lSplit"));
-    roads.push(makeSprite(init[0]+256,init[1]+128,"tbTile"));
-    roads.push(makeSprite(init[0]+256,init[1]-128,"tbTile"));
-    roads.push(makeSprite(init[0]+256,init[1]-256,"brTile"));
-    roads.push(makeSprite(init[0]+256,init[1]+256,"trTile"));
-    for(i=0;i<Math.ceil((window.innerWidth - init[0]+384)/128);i++)
+    if(player == 0)
     {
-        roads.push(makeSprite(init[0]+384+(i*128),init[1]+256,"lrTile"));
-        roads.push(makeSprite(init[0]+384+(i*128),init[1]-256,"lrTile"));
+        roads.push(makeSprite(init[0]+128,init[1],"lrTile"));
+        roads.push(makeSprite(init[0]+256,init[1],"lSplit"));
+        roads.push(makeSprite(init[0]+256,init[1]+128,"tbTile"));
+        roads.push(makeSprite(init[0]+256,init[1]-128,"tbTile"));
+        roads.push(makeSprite(init[0]+256,init[1]-256,"brTile"));
+        roads.push(makeSprite(init[0]+256,init[1]+256,"trTile"));
+        for(i=0;i<Math.ceil((window.innerWidth - init[0]+384)/128);i++)
+        {
+            roads.push(makeSprite(init[0]+384+(i*128),init[1]+256,"lrTile"));
+            roads.push(makeSprite(init[0]+384+(i*128),init[1]-256,"lrTile"));
+        }
+    }else{
+        roads.push(makeSprite(init[0]-256,init[1],"lrTile"));
+        roads.push(makeSprite(init[0]-384,init[1],"rSplit"));
+        roads.push(makeSprite(init[0]-384,init[1]-128,"tbTile"));
+        roads.push(makeSprite(init[0]-384,init[1]+128,"tbTile"));
+        roads.push(makeSprite(init[0]-384,init[1]+256,"ltTile"));
+        roads.push(makeSprite(init[0]-384,init[1]-256,"lbTile"));
+        for(i=0;i<Math.ceil((window.innerWidth - init[0]+512)/128);i++)
+        {
+            roads.push(makeSprite(init[0]-512-(i*128),init[1]+256,"lrTile"));
+            roads.push(makeSprite(init[0]-512-(i*128),init[1]-256,"lrTile"));
+        }
     }
 }
 function makeCastle(xInit)
@@ -80,10 +110,20 @@ function makeCastle(xInit)
     var castle = new Array(2);
     castle[0] = new PIXI.Sprite(PIXI.loader.resources["static/assets/main/castle1.png"].texture);
     castle[0].y = center + (castle[0].height/2) - 64;
-    castle[0].x = xInit + 5; 
+    if(player ==0)
+    {
+        castle[0].x = xInit + 5; 
+    }else{
+        castle[0].x = xInit - 128 -5; 
+    }
     castle[1] = new PIXI.Sprite(PIXI.loader.resources["static/assets/main/castle2.png"].texture);
     castle[1].y = castle[0].y - castle[1].height; 
-    castle[1].x = xInit + 5 ;
+    if(player == 0)
+    {
+        castle[1].x = xInit + 5 ;
+    }else{
+        castle[1].x = xInit - 128 -5;
+    }
     stage.addChild(castle[0]);
     stage.addChild(castle[1]);
     return castle[0].y - 30;   
