e3872b235cde320fdc028739954afd1a83e35f74
Jonathan Balls <j.balls@btopenworld.com>
2016-10-08 05:41:28 +0200

    Started work on splash screen

---
+++ static/splash/splash.js
@@ -0,0 +1,20 @@
+function showNewGameForm() {
+    document.getElementById("joinOrCreateForm").style.display = "none";
+    document.getElementById("createGameForm").style.display = "block";
+}
+
+function showJoinGameForm() {
+    document.getElementById("joinOrCreateForm").style.display = "none";
+    document.getElementById("joinGameForm").style.display = "block";
+}
+
+function createNewGame() {
+    var name = document.getElementsByName("createGameName")[0].value;
+    console.log("Creating new game with name " + name);
+}
+
+function joinGame() {
+    var name = document.getElementsByName("joinGameName")[0].value;
+    var gameCode = document.getElementsByName("joinGameCode")[0].value;
+    console.log("Creating new game with name " + name + " and code " + gameCode);
+}
